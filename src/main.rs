2dc3e23ccfe4c14f744ef7750abd674481988c29
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2023-06-04 13:06:19 +0530

    v1 impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rgrep::Config;
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing the arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = rgrep::run(config) {
+        eprintln!("Application Error: {e}");
+        process::exit(1);
+    }
 }
